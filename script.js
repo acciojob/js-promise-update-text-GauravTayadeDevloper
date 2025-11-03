c100bdcdbb3f7efa113fc588586767ac85ea75da
GAURAV  TAYADE <110455312+GauravTayadeDevloper@users.noreply.github.com>
2025-11-03 16:12:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+function  promis(str) {
+let op = document.getElementById("output");
+return new Promis ((resolve)=>{
+	setTimeout(()=>{
+		
+		op.innerHTML =`<p>${str}</p>`;
+		resolve("Hello, world!");
+	},1000)
+})	
+}
+promis("Hello, world!");
+
